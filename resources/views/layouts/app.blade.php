d1f0e3f7aadb6c09ded2a70541b6dcd17ac570b7
DennyNice <dennynice@icloud.com>
2021-04-12 19:00:31 +0200

    My first App

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<title>My first App</title>
+</head>
+<body>
+<div class="container">
+    <nav class="navbar navbar-default">
+
+    </nav>
+</div>
+@yield('content')
+</body>
+</html>
